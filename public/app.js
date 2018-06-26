accaf1e67f85e7527da10bbca4da575a5909fced
KUBIX90 <mark.j.kubik@gmail.com>
2018-06-26 22:51:28 +0100

    Added scroll functionality to navbar

--- public/app.js
+++ public/app.js
@@ -1,3 +1,15 @@
 document.querySelector("#menu-icon").addEventListener("click", function(){
     document.querySelector("#nav-menu nav ul").classList.toggle("menu-visible");
 });
+
+var nav = document.querySelector("#nav-menu");
+
+window.addEventListener("scroll", function(){
+
+    if(window.pageYOffset > 600){
+        nav.classList.add("bg-blck");
+    } else {
+        nav.classList.remove("bg-blck");
+    }
+
+});
