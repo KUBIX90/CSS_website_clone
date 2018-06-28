840df9023c0c4600e449070c3633812d9f74c786
KUBIX90 <mark.j.kubik@gmail.com>
2018-06-28 23:11:56 +0100

    Added carousel to testimonial section

--- public/app.js
+++ public/app.js
@@ -2,6 +2,8 @@ document.querySelector("#menu-icon").addEventListener("click", function(){
     document.querySelector("#nav-menu nav ul").classList.toggle("menu-visible");
 });
 
+///////////////////////////////////////////////////////////////////////
+
 var nav = document.querySelector("#nav-menu");
 
 window.addEventListener("scroll", function(){
@@ -13,3 +15,26 @@ window.addEventListener("scroll", function(){
     }
 
 });
+
+///////////////////////////////////////////////////////////////////////
+
+var slideIndex = 0;
+carousel();
+
+function carousel(i) {
+    
+    var x = document.querySelectorAll(".profiles article");
+    var y = document.querySelectorAll(".indicators div");
+
+    
+    for (i = 0; i < x.length; i++) {
+      y[i].classList.remove("active");
+      x[i].classList.remove("profile-active"); 
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].classList.add("profile-active");
+    y[slideIndex-1].classList.add("active");
+    setTimeout(carousel, 4000);
+
+}
