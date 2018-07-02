b11edbfaeb8870dbd64606ab829971176b3c5ad0
KUBIX90 <mark.j.kubik@gmail.com>
2018-07-02 22:08:05 +0100

    Added functionality to carousel

--- public/app.js
+++ public/app.js
@@ -13,28 +13,30 @@ window.addEventListener("scroll", function(){
     } else {
         nav.classList.remove("bg-blck");
     }
-
 });
 
 ///////////////////////////////////////////////////////////////////////
 
-var slideIndex = 0;
-carousel();
+var slideIndex = 1;
+var x = document.querySelectorAll(".profiles article");
+var y = document.querySelectorAll(".indicators div");
+var timeOut = setInterval(carousel, 4000);
 
-function carousel(i) {
-    
-    var x = document.querySelectorAll(".profiles article");
-    var y = document.querySelectorAll(".indicators div");
+function carousel() {
 
-    
-    for (i = 0; i < x.length; i++) {
-      y[i].classList.remove("active");
-      x[i].classList.remove("profile-active"); 
-    }
-    slideIndex++;
-    if (slideIndex > x.length) {slideIndex = 1}
-    x[slideIndex-1].classList.add("profile-active");
-    y[slideIndex-1].classList.add("active");
-    setTimeout(carousel, 4000);
+  for (i = 0; i < x.length; i++) {
+    y[i].classList.remove("active");
+    x[i].classList.remove("profile-active");
+  }
+
+  x[slideIndex].classList.add("profile-active");
+  y[slideIndex].classList.add("active");
+
+  slideIndex = (slideIndex += 1) % x.length
 
 }
+
+function changeInfo(n){ 
+    slideIndex = n;
+    carousel();
+};
