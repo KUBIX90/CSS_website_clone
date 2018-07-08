d1d68a6e417fb551e379872ad729ce75ff31bde3
KUBIX90 <mark.j.kubik@gmail.com>
2018-07-08 22:14:53 +0100

    Made image click events to show modal

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-document.querySelector("#menu-icon").addEventListener("click", function(){
+document.querySelector("#menu-icon").addEventListener("click", function () {
     document.querySelector("#nav-menu nav ul").classList.toggle("menu-visible");
 });
 
@@ -6,9 +6,9 @@ document.querySelector("#menu-icon").addEventListener("click", function(){
 
 var nav = document.querySelector("#nav-menu");
 
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function () {
 
-    if(window.pageYOffset > 600){
+    if (window.pageYOffset > 600) {
         nav.classList.add("bg-blck");
     } else {
         nav.classList.remove("bg-blck");
@@ -24,25 +24,26 @@ var timeOut = setInterval(carousel, 4000);
 
 function carousel() {
 
-  for (i = 0; i < x.length; i++) {
-    y[i].classList.remove("active");
-    x[i].classList.remove("profile-active");
-  }
+    for (i = 0; i < x.length; i++) {
+        y[i].classList.remove("active");
+        x[i].classList.remove("profile-active");
+    }
 
-  x[slideIndex].classList.add("profile-active");
-  y[slideIndex].classList.add("active");
+    x[slideIndex].classList.add("profile-active");
+    y[slideIndex].classList.add("active");
 
-  slideIndex = (slideIndex += 1) % x.length
+    slideIndex = (slideIndex += 1) % x.length
 
 }
 
-function changeInfo(n){ 
+function changeInfo(n) {
     slideIndex = n;
     carousel();
 };
 
 ///////////////////////////////////////////////////////////////////////
 
+var imgOverlays = document.querySelectorAll(".imgs section div");
 var images = document.querySelectorAll("#gallery .imgs section");
 var buttons = document.querySelectorAll("#gallery .btns button");
 var all = document.querySelector(".btn-all");
@@ -50,76 +51,77 @@ var lorem = document.querySelector(".btn-lorem");
 var dolar = document.querySelector(".btn-dolar");
 var ipsum = document.querySelector(".btn-ipsum");
 
-function filterImages(btn, class1, class2){
-    for(i = 0; i < images.length; i++){
+function filterImages(btn, class1, class2) {
+    for (i = 0; i < images.length; i++) {
         btn.classList.add("btn-active");
         images[i].classList.remove("display-none");
-        if(images[i].classList.contains(class1) || images[i].classList.contains(class2) ){
+        if (images[i].classList.contains(class1) || images[i].classList.contains(class2)) {
             images[i].classList.add("display-none");
         }
     }
 }
 
-function removeActiveBtn(){
-    for(i = 0; i < buttons.length; i++){
+function removeActiveBtn() {
+    for (i = 0; i < buttons.length; i++) {
         buttons[i].classList.remove("btn-active");
     };
 }
 
-all.addEventListener("click", function(){
+all.addEventListener("click", function () {
     removeActiveBtn();
-    for(i = 0; i < images.length; i++){
+    for (i = 0; i < images.length; i++) {
         all.classList.add("btn-active");
-        if(images[i].classList.contains("display-none")){
+        if (images[i].classList.contains("display-none")) {
             images[i].classList.remove("display-none")
         }
     }
 });
 
-lorem.addEventListener("click", function (){
+lorem.addEventListener("click", function () {
     removeActiveBtn();
     filterImages(lorem, "dolar", "ipsum");
 });
 
-dolar.addEventListener("click", function (){
+dolar.addEventListener("click", function () {
     removeActiveBtn();
     filterImages(dolar, "ipsum", "lorem");
 });
 
-ipsum.addEventListener("click", function (){
+ipsum.addEventListener("click", function () {
     removeActiveBtn();
     filterImages(ipsum, "dolar", "lorem");
 });
 
+document.querySelector("#gallery .imgs").addEventListener("click", function (e) {
+    for (i = 0; i < imgOverlays.length; i++) {
+        if (e.target == imgOverlays[i]) {
+            modal.style.display = "flex";
+            mainImg.src = e.target.previousElementSibling.src;
+        }
+    }
+});
+
 ///////////////////////////////////////////////////////////////////////
 
-var mainImg = document.querySelector(".img-main");
+var mainImg = document.querySelector("#modal .modal-content .img-main");
 var allModalImgs = document.querySelectorAll("#modal .modal-content img");
 var modal = document.querySelector("#modal .modal-background");
 var opacity = 0.4;
 
-function resetOpacity(){
-    for(i = 0; i < allModalImgs.length; i++){
+function resetOpacity() {
+    for (i = 0; i < allModalImgs.length; i++) {
         allModalImgs[i].style.opacity = 1;
     }
 }
 
-for(i = 0; i < images.length; i++){
-    images[i].addEventListener("click", function(){
-        modal.style.display = "flex";
-        mainImg.src = e.target.src;
-        e.target.style.opacity = opacity;
-    });
-};
-
-for(i = 0; i < allModalImgs.length; i++){
-    allModalImgs[i].addEventListener("click", function(e){
+for (i = 0; i < allModalImgs.length; i++) {
+    allModalImgs[i].addEventListener("click", function (e) {
         resetOpacity();
         mainImg.src = e.target.src;
         e.target.style.opacity = opacity;
     });
 };
 
-document.querySelector(".close").addEventListener("click", function(){
+document.querySelector(".close").addEventListener("click", function () {
     modal.style.display = "none";
 });
