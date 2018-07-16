0fb315bd47ac8c4c364273b02aceb1de9f280a28
KUBIX90 <mark.j.kubik@gmail.com>
2018-07-16 21:15:20 +0100

    Amended SASS file structure & gulp watch task's error handling

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
 
 gulp.task('build-sass', function () {
-    gulp.src('./public/sass/*.scss')
+    gulp.src('./public/sass/main.scss')
+        .pipe(plumber())
         .pipe(sass())
         .pipe(gulp.dest('./public/css'))
 })
 
 gulp.task('watch', ['build-sass'], function () {
-    gulp.watch('./public/sass/*.scss', ['build-sass']);
+    gulp.watch('./public/sass/**/*.scss', ['build-sass']);
 });
