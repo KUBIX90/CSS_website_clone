4a71f8d83afa1530a7a00fca22493c5d59798ea0
KUBIX90 <mark.j.kubik@gmail.com>
2018-08-14 23:33:02 +0100

    Minified CSS using gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var plumber = require('gulp-plumber');
+var cleanCSS = require('gulp-clean-css');
 
 gulp.task('build-sass', function () {
     gulp.src('./public/sass/main.scss')
@@ -8,6 +9,18 @@ gulp.task('build-sass', function () {
         .pipe(sass())
         .pipe(gulp.dest('./public/css'))
 })
+
+gulp.task('clean-css', () => {
+    return gulp.src('./public/css/main.css')
+        .pipe(cleanCSS({
+            debug: true,
+            rebase: false
+        }, (details) => {
+            console.log(`${details.name}: ${details.stats.originalSize}`);
+            console.log(`${details.name}: ${details.stats.minifiedSize}`);
+        }))
+        .pipe(gulp.dest('./dist/css'));
+});
 
 gulp.task('watch', ['build-sass'], function () {
     gulp.watch('./public/sass/**/*.scss', ['build-sass']);
